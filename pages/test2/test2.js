da9bb976744c10464d2bc007ed0b2aeea32336ea
XLZF <hou3125378@live.com>
2017-10-17 18:11:39 +0800

    又下课了~！！

--- pages/test2/test2.js
+++ pages/test2/test2.js
@@ -1,5 +1,6 @@
 var order = ["a","b","c","d","e"];
 var index = 0;
+var progressNum = 0;
 Page({
 
   /**
@@ -13,7 +14,34 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
     inter:1000,
-    autoplay:true
+    autoplay:true,
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ],
+    progressVal:0,
+    country:[
+      {name: 'USA', value: '美国'},
+      {name: 'CHN', value: '中国', checked: 'true'},
+      {name: 'BRA', value: '巴西'},
+      {name: 'JPN', value: '日本'},
+      {name: 'ENG', value: '英国'},
+      {name: 'TUR', value: '法国'},
+    ]
+  },
+  inputFn:function(e){
+    var a = e.detail.value;
+    console.log(e.detail.value);
+    return a;
+  },
+  inputfocusFn:function(e){
+    console.log("聚焦了！");
+  },
+  inputBlurFn:function(){
+    console.log("失去焦点了！");
   },
   scrolltoupper:function(e){
     console.log(e);
@@ -48,11 +76,31 @@ Page({
       autoplay:!this.data.autoplay
     });
   },
+  subFn:function(){
+    console.log("提交表单");
+  },
+  resetFn:function(){
+    console.log("点击了重置按钮！");
+  },
+  CheckChange:function(e){
+    console.log(e.detail.value);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that =this;
+    var timer = setInterval(function(){
+      progressNum++;
+      if(progressNum>=100)
+      {
+        clearInterval(timer);
+        //progressNum=0;
+      }
+      that.setData({
+        progressVal:progressNum
+      });
+    },30)
   },
 
   /**
